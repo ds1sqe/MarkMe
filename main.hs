07e73e8202fcab49f790400bf69d8b574fdb3b4a
ds1sqe <ds1sqe@mensakorea.org>
2023-01-04 14:13:44 +0900

    modified main

--- main.hs
+++ main.hs
@@ -1,16 +1,58 @@
-main = putStrLn html
+-- import
 
-html = createHtml "Hello title" "Hello content"
+-- type
 
-createHtml title content = tagHtml (tagHead (tagTitle (tagH1 title)) <> tagBody (tagP content))
+newtype Html = Html String
+newtype Structure = Structure String
+
+type Title = String
+
+-- monoid
+
+-- functions
+
+getStringFromStructure :: Structure -> String
+getStringFromStructure (Structure str) = str
 
 element :: String -> String -> String
 element tag content =
     "<"<> tag <> ">" <> content <> "</" <> tag <> ">"
 
-tagHtml = element "html"
-tagBody = element "body"
-tagHead = element "head"
-tagTitle = element "title"
-tagP = element "p"
-tagH1 = element "h1"
+appendTag :: Structure -> Structure -> Structure
+appendTag (Structure a) (Structure b) =
+    Structure (a <> b)
+
+-- Html wrapper
+tagHtml :: Title -> Structure -> Html
+tagHtml title content =
+    Html (
+      element "html" (
+        element "head" (
+          element "title" title) <>
+        element "body" ( getStringFromStructure content)
+     ))
+
+-- tags
+tagP:: String -> Structure
+tagP = Structure . element "p"
+
+tagH1 :: String -> Structure
+tagH1 = Structure .  element "h1"
+
+render :: Html -> String
+render html =
+    case html of
+        Html str -> str
+
+--
+html :: Html
+html =
+  tagHtml "Title"
+    (appendTag ( tagH1 "Heading 1")
+        ( appendTag (tagP "Paragraph 1") (tagP "Paragraph 2"))
+    )
+
+-- main
+
+main :: IO ()
+main = putStrLn (render html)
