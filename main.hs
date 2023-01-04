f051e160f3e8897ce9845f836c3b804fd9da5a4e
ds1sqe <ds1sqe@mensakorea.org>
2023-01-04 15:00:56 +0900

    divided html module, and implemented parsing escape character

--- main.hs
+++ main.hs
@@ -1,58 +1,14 @@
 -- import
-
--- type
-
-newtype Html = Html String
-newtype Structure = Structure String
-
-type Title = String
-
+import           Html
 -- monoid
 
--- functions
-
-getStringFromStructure :: Structure -> String
-getStringFromStructure (Structure str) = str
-
-element :: String -> String -> String
-element tag content =
-    "<"<> tag <> ">" <> content <> "</" <> tag <> ">"
-
-appendTag :: Structure -> Structure -> Structure
-appendTag (Structure a) (Structure b) =
-    Structure (a <> b)
-
--- Html wrapper
-tagHtml :: Title -> Structure -> Html
-tagHtml title content =
-    Html (
-      element "html" (
-        element "head" (
-          element "title" title) <>
-        element "body" ( getStringFromStructure content)
-     ))
-
--- tags
-tagP:: String -> Structure
-tagP = Structure . element "p"
-
-tagH1 :: String -> Structure
-tagH1 = Structure .  element "h1"
-
-render :: Html -> String
-render html =
-    case html of
-        Html str -> str
-
---
+-- main
 html :: Html
 html =
-  tagHtml "Title"
-    (appendTag ( tagH1 "Heading 1")
-        ( appendTag (tagP "Paragraph 1") (tagP "Paragraph 2"))
+  htmlTag "Title"
+    (appendTag ( h1Tag "Heading 1")
+        ( appendTag (pTag "Paragraph 1") (pTag "Paragraph 2"))
     )
-
--- main
 
 main :: IO ()
 main = putStrLn (render html)
